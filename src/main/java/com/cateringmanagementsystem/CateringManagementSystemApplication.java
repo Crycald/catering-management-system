29db6ad055d8a0f225ea3868c34f50e48510a5f9
Crycald <krzysiekdybikowski@gmail.com>
2020-07-21 15:24:14 +0200

    basic files

---
+++ src/main/java/com/cateringmanagementsystem/CateringManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.cateringmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CateringManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CateringManagementSystemApplication.class, args);
+    }
+
+}
